57fcde3ddf8afac3d75a6ace6c700b82be3452b7
Julian Mayorga <mayorga.julian@gmail.com>
2012-12-08 19:41:53 -0300

    Fetch urls from in-memory url queue

--- main.c
+++ main.c
@@ -4,57 +4,78 @@
 #include <string.h>
 
 #include "net.h"
+#include "list.h"
 #include "file.h"
 #include "thpool.h"
 
-#define WORK_TOTAL 1
+#define WORK_TOTAL 3
 
 pthread_mutex_t work_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-struct work_args {
+struct url_list {
   char* host;
   char* url;
-  int done_work;
+  struct list_head list;
   pthread_mutex_t* mutex;
 };
 
-void crawl(struct work_args *args) {
+void crawl(struct url_list *url_list) {
   char* buffer = NULL;
+  struct url_list* url_list_ptr = (struct url_list*) url_list;
+  struct url_list* tmp_ptr;
 
-  //fetch url
-  buffer = net_fetch(args->host, args->url);
-  pthread_mutex_lock(args->mutex);
-   //save file to disk
-  if ( -1 == file_save(buffer, args->host, args->url) ) {
-    perror("file_save");
-    exit(1);
+  if (list_empty(&url_list_ptr->list) != 1) {
+    printf("Mutex:%d\n", (int)url_list_ptr->mutex);
+    pthread_mutex_lock(url_list_ptr->mutex);
+    tmp_ptr = list_entry(url_list_ptr->list.prev, struct url_list, list);
+    //fetch url
+    buffer = net_fetch(tmp_ptr->host, tmp_ptr->url);
+    //save file to disk
+    if ( -1 == file_save(buffer, tmp_ptr->host, tmp_ptr->url) ) {
+      perror("file_save");
+      exit(1);
+    }
+    list_del(&tmp_ptr->list);
+    free(tmp_ptr);
+    pthread_mutex_unlock(url_list->mutex);
   }
-  args->done_work++;
-  pthread_mutex_unlock(args->mutex);
 }
 
 int main(){
   int i;
+  struct url_list url_list, *url_list_ptr, *url_list_ptr2, *url_list_ptr3;
   char* host = "www.google.com";
   char* url = "/index.html";
 
-  struct work_args* args;
-
-  args= (struct work_args*)malloc(sizeof(struct work_args)); 
-
-  args->mutex = &work_mutex;
-  args->done_work = 0;
-  args->host = host;
-  args->url = url;
+  INIT_LIST_HEAD(&url_list.list);
 
   thpool_t* threadpool;             /* make a new thread pool structure     */
   threadpool=thpool_init(2);        /* initialise it to x number of threads */
 
+  url_list.mutex = &work_mutex;
+
+  url_list_ptr = (struct url_list *)malloc(sizeof(struct url_list));
+  url_list_ptr->host = host;
+  url_list_ptr->url = url;
+
+  list_add(&(url_list_ptr->list), &(url_list.list));
+
+  url_list_ptr2 = (struct url_list *)malloc(sizeof(struct url_list));
+  url_list_ptr2->host = "www.altavista.com";
+  url_list_ptr2->url = "/index.html";
+
+  list_add(&(url_list_ptr2->list), &(url_list.list));
+
+  url_list_ptr3 = (struct url_list *)malloc(sizeof(struct url_list));
+  url_list_ptr3->host = "www.guialocal.com.ar";
+  url_list_ptr3->url = "/index.html";
+
+  list_add(&(url_list_ptr3->list), &(url_list.list));
   for (i=0; i<WORK_TOTAL; i++){
-    thpool_add_work(threadpool, (void*)crawl, (void*)args);
+    thpool_add_work(threadpool, (void*)crawl, (void*)&url_list);
   };
 
-  while (WORK_TOTAL > args->done_work) {
+  while (list_empty(&url_list.list) != 1) {
     sleep(1);
   }
 
