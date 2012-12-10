47f6e3d337597fd9fcae2ffa2f1124ce03f6beef
Julian Mayorga <mayorga.julian@gmail.com>
2012-12-10 01:24:05 -0300

    Add file.c test

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 CC = gcc
 CFLAGS = -Wall -pthread -g -ggdb
 NOMBRE = spider
-TARGETS = spider
+TARGETS = spider test_file
 
 all: $(TARGETS)
 
 spider: main.o thpool.o net.o file.o
+	$(CC) $(CFLAGS) $(STANDARD) -o $@ $^ $(LDLIBS)
+
+test_file: test_file.o file.o
 	$(CC) $(CFLAGS) $(STANDARD) -o $@ $^ $(LDLIBS)
 
 clean:
---
+++ test_file.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "minunit.h"
+#include "file.h"
+
+int tests_run = 0;
+
+char* dir = "new_dir";
+char* filename = "/test.txt";
+
+void tear_down() {
+  if (unlink("sites/new_dir/test.txt") == -1)
+    perror("unlink");
+  if (rmdir("sites/new_dir") == -1)
+    perror("rmdir");
+}
+
+static char* test_file_save() {
+  char *content = "This is a test file";
+  mu_assert("error, couldn't create and save the file", file_save(content, dir, filename) != -1);
+  return 0;
+}
+
+static char* all_tests() {
+  mu_run_test(test_file_save);
+  return 0;
+}
+
+int main(int argc, char** argv) {
+  char* result = all_tests();
+  if (result != 0) {
+    printf("%s\n", result);
+  } else {
+    printf("ALL TESTS PASSED\n");
+  }
+  printf("Tests run: %d\n", tests_run);
+
+  tear_down();
+
+  return result != 0;
+}
